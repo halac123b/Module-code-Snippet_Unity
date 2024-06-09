98959cac944785b4a57637f251de6b0b34d154a4
halac123b <halac123b@gmail.com>
2024-06-09 19:07:33 +0700

    Add m_9

---
+++ 9/BaseState.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseState<EState>
+{
+    public abstract void EnterState();
+    public abstract void ExitState();
+    public abstract void UpdateState();
+    public abstract EState GetNextState();
+    public abstract void OnTriggerEnter();
+    public abstract void OnTriggerStay();
+    public abstract void OnTriggerExit();
+}
--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Table of content to look up on branches: <br>
 6. Video player <br>
 7. Implement multiple spells from one ScriptableObject class <br>
 8. JsonUtility, tool of Unity to handle Json data <br>
+9. StateMachine <br>
